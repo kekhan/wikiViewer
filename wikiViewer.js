4c6eb463bef72aece6c8c6e1b25a87f2fa1c281a
Kerin Khan <kerink585@gmail.com>
2017-11-03 14:43:09 -0700

    fix search prompt

---
+++ wikiViewer.js
@@ -0,0 +1,50 @@
+var myRequest = new XMLHttpRequest();
+
+function getWikiInfo(){
+	var val = prompt("Search wikipedia");
+	document.getElementById("opened").innerHTML= "wikipedia Search: "+val;	
+	var myRequest = new XMLHttpRequest();
+	myRequest.onreadystatechange = function(){
+		if (myRequest.readyState === XMLHttpRequest.DONE)
+		{
+			if (myRequest.status < 400)
+
+			{
+				var str = JSON.parse(myRequest.responseText);
+				wikiHtmlCall(str);
+
+			}
+		}
+	};
+
+
+	var url = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + val + '&format=json&origin=*';
+	myRequest.open('GET',url,true);
+	myRequest.send(null);-1
+}
+function wikiHtmlCall(data){
+	document.getElementById("textLoad").innerHTML ="Reload to Search Again"
+	var articleTitles =data[1];
+	var webUrl= data[data.length-1]
+	var articleInfo = data[2];
+	var str="";
+	var anchorElem;
+	var newelem;
+	for(var i=0;i<articleTitles.length;i++){
+		var ul = document.getElementById("listItems");
+	    var li = document.createElement("li");
+
+		str= articleTitles[i]+': '+articleInfo[i]+"\r";
+		anchorElem = document.createElement('a');
+        anchorElem.setAttribute("href", webUrl[i]);
+        anchorElem.innerHTML = str;
+        li.appendChild(anchorElem);
+        ul.appendChild(li);
+       
+        //document.body.appendChild(anchorElem);
+
+
+		
+	}
+	
+}
